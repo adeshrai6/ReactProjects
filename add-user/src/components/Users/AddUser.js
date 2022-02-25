2bde9e81d89ab3953a51633ad265bbf6b76d0c9a
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-02-24 21:59:55 -0500

    Adding form for username and age input

--- add-user/src/components/Users/AddUser.js
+++ add-user/src/components/Users/AddUser.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const AddUser = props => {
-    <form>
+    <form onSubmit={}>
         <label>Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
     </form>
 };
 
