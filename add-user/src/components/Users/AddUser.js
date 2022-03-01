6af30c49e1d4e4e96e1d333e465549a14e89f427
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-02-28 22:10:43 -0500

    handaling error dynamically

--- add-user/src/components/Users/AddUser.js
+++ add-user/src/components/Users/AddUser.js
@@ -6,13 +6,22 @@ import Button from "../UI/Button";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values.'
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (>0).'
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -30,7 +39,7 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title ="Error Occured!" message = "Something went Wrong" /> 
+      {error && <ErrorModal title ={error.title} message = {error.message} /> }
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
