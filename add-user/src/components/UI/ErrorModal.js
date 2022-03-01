0593ac641ce0d918a806b608041557cca9c5447a
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-02-28 22:02:38 -0500

    using backdrop class to dime back content

--- add-user/src/components/UI/ErrorModal.js
+++ add-user/src/components/UI/ErrorModal.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
-import classes from "./ErrorModal.css";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
+    <div>
+      <div className={classes.backdrop}/>
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -16,6 +18,7 @@ const ErrorModal = (props) => {
           <Button>Okay</Button>
         </footer>
       </Card>
+      </div>
   );
 };
 
