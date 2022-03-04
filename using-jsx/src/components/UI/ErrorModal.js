e8d042292326279223964cd549904f9c0c173492
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-03 22:25:14 -0500

    using ReactDOM to create portal overlay and backdrop

--- using-jsx/src/components/UI/ErrorModal.js
+++ using-jsx/src/components/UI/ErrorModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ReactDOM from 'react-dom';
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
@@ -25,9 +25,10 @@ const ModalOverlay = (props) => {
 };
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+      {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('overlay-root'))}
+    </React.Fragment>
   );
 };
 
